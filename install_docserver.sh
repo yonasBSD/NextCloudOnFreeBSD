fa0279b416b59167856778b75f8e3b6802980b6c
Gibran Khan <g.khan@posteo.net>
2024-04-14 20:51:37 +0200

    updated `install_docserver.sh`

--- install_docserver.sh
+++ install_docserver.sh
@@ -2,13 +2,20 @@
 
 #
 # Install Document Server add-on for NextCloud
-# This is untested on NC27
-# Last update: 2023-07-23
+# This is untested on NC28; please report issues
+# Last update: 2024-04-14
 # https://github.com/theGeeBee/NextCloudOnFreeBSD/
 #
 
 # Load config settings
-. install.conf
+CONFIG_FILE="${PWD}/install.conf"
+
+if [ -f "$CONFIG_FILE" ]; then
+    . "$CONFIG_FILE"
+else
+    echo "Config file '$CONFIG_FILE' not found."
+    exit 1
+fi
 
 # Check for root privileges
 if [ "$(id -u)" -ne 0 ]; then
@@ -24,7 +31,7 @@ else
     lib32=false
 fi
 
-# Check if HBSD is present in uname string ###
+# Check if HBSD is present in uname string
 hbsd_test=$(uname -a | grep -o 'HBSD')
 
 # Verify FreeBSD with lib32 support
@@ -53,10 +60,14 @@ service linux start
 echo "Installing Document Server for Nextcloud."
 
 # ONLYOFFICE
-sudo -u www php "${WWW_DIR}/nextcloud/occ" app:enable onlyoffice
-sudo -u www php "${WWW_DIR}/nextcloud/occ" app:install documentserver_community
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" app:enable onlyoffice
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" app:install documentserver_community
 
 # set ONLYOFFICE to accept the self-signed certificate and point it to ${MY_IP} instead of localhost
 # ${HOST_NAME} would work instead if DNS is set up correctly
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:app:set onlyoffice verify_peer_off --value="true"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:app:set onlyoffice DocumentServerUrl --value="https://${MY_IP}/apps/documentserver_community/"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:app:set onlyoffice verify_peer_off --value="true"
+if [ "$USE_HOSTNAME" = "true" ]; then
+    sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:app:set onlyoffice DocumentServerUrl --value="https://${HOST_NAME}/apps/documentserver_community/"
+else
+    sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:app:set onlyoffice DocumentServerUrl --value="https://${MY_IP}/apps/documentserver_community/"
+fi
